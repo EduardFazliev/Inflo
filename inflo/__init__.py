f8daa29d8c53a29a390b7e1bff830e93b46cfe28
efazliev <efazliev@infotech.team>
2016-07-30 19:49:34 +0300

    Add server_list to init.py.

--- inflo/__init__.py
+++ inflo/__init__.py
@@ -1,6 +1,6 @@
 from inflo.helpers import set_conf, get_conf
-from inflo.inflo_commands import get_tenant_info
+from inflo.inflo_commands import get_tenant_info, server_list
 
 __all__ = [
-    'set_conf', 'get_conf', 'get_tenant_info'
+    'set_conf', 'get_conf', 'get_tenant_info', 'server_list'
 ]
