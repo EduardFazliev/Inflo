77b22bfbbfa39df7647ccf1a79fcd9e1c019be4c
efazliev <efazliev@infotech.team>
2016-07-30 20:26:49 +0300

    Add os-list function.

--- inflo/__init__.py
+++ inflo/__init__.py
@@ -1,6 +1,6 @@
 from inflo.helpers import set_conf, get_conf
-from inflo.inflo_commands import get_tenant_info, server_list
+from inflo.inflo_commands import get_tenant_info, server_list, os_list
 
 __all__ = [
-    'set_conf', 'get_conf', 'get_tenant_info', 'server_list'
+    'set_conf', 'get_conf', 'get_tenant_info', 'server_list', 'os_list'
 ]
--- inflo/inflo_commands.py
+++ inflo/inflo_commands.py
@@ -66,5 +66,32 @@ def server_list(api_key=None, customer_id=None, raw=False):
         print message
 
 
+def os_list(api_key=None, customer_id=None):
+    url = api_link + 'distribution'
+    if api_key or customer_id is None:
+        api_key, customer_id = helpers.get_conf()
+
+    payload = {'clientId': customer_id, 'apiKey': api_key}
+
+    code, message = requests_lib.send_get_request(url, payload)
+
+    answer = json.loads(message)
+
+    print '''
+    -> Status: {0}
+    -> Printing result...
+    '''.format(answer['status'])
+
+    pt = prettytable.PrettyTable()
+    result = answer['result']
+
+    pt.add_column('id', [server['id'] for server in result])
+    pt.add_column('name', [server['name'] for server in result])
+    pt.add_column('description', [server['description'] for server in result])
+    pt.add_column('bitness', [server['bitness'] for server in result])
+
+    print pt
+
+
 if __name__ == '__main__':
     pass
