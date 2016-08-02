ecf5845407548c53e88eba2bbef891e6eabce22e
Sergei Zyubin <s.zyubin@infotech.team>
2016-08-02 10:06:32 +0100

    Fix indents, Remove inobvious -m switch for vm-id, Use format() for url.

--- inflo/inflo_commands.py
+++ inflo/inflo_commands.py
@@ -4,12 +4,11 @@ import prettytable
 import helpers
 import requests_lib
 
-
 api_link = 'https://api.flops.ru/api/v1/'
 
 
 def get_tenant_info(api_key=None, customer_id=None):
-    url = api_link + 'tenant'
+    url = '{0}/tenant/'.format(api_link)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -34,7 +33,7 @@ def get_tenant_info(api_key=None, customer_id=None):
 
 
 def server_list(api_key=None, customer_id=None, raw=False):
-    url = api_link + 'vm'
+    url = '{0}/vm/'.format(api_link)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -67,7 +66,7 @@ def server_list(api_key=None, customer_id=None, raw=False):
 
 
 def os_list(api_key=None, customer_id=None):
-    url = api_link + 'distribution'
+    url = '{0}/distribution/'.format(api_link)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -94,7 +93,7 @@ def os_list(api_key=None, customer_id=None):
 
 
 def get_vm_info(api_key=None, customer_id=None, vm_id=str, raw=False):
-    url = api_link + 'vm/' + vm_id
+    url = '{0}/vm/{1}/'.format(api_link, vm_id)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -110,7 +109,7 @@ def get_vm_info(api_key=None, customer_id=None, vm_id=str, raw=False):
 
     if not raw:
 
-        pt= prettytable.PrettyTable()
+        pt = prettytable.PrettyTable()
         result = answer['result']
 
         pt.add_column('id', [server['id'] for server in result])
@@ -131,7 +130,7 @@ def get_vm_info(api_key=None, customer_id=None, vm_id=str, raw=False):
 
 
 def get_vm_snapshots(api_key=None, customer_id=None, vm_id=str):
-    url = api_link + 'vm/' + vm_id + '/snapshots'
+    url = '{0}/vm/{1}/snapshots/'.format(api_link, vm_id)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -145,19 +144,19 @@ def get_vm_snapshots(api_key=None, customer_id=None, vm_id=str):
     -> Printing result...
     '''.format(answer['status'])
 
-        pt= prettytable.PrettyTable()
-        result = answer['result']
+    pt = prettytable.PrettyTable()
+    result = answer['result']
 
-        pt.add_column('id', [server['id'] for server in result])
-        pt.add_column('name', [server['name'] for server in result])
-        pt.add_column('description', [server['description'] for server in result])
-        pt.add_column('parentSnapshotId', [server['parentSnapshotId'] for server in result])
-        # TODO: Convert Unix time to standard one.
-        pt.add_column('timeAdded', [server['timeAdded'] for server in result])
+    pt.add_column('id', [server['id'] for server in result])
+    pt.add_column('name', [server['name'] for server in result])
+    pt.add_column('description', [server['description'] for server in result])
+    pt.add_column('parentSnapshotId', [server['parentSnapshotId'] for server in result])
+    # TODO: Convert Unix time to standard one.
+    pt.add_column('timeAdded', [server['timeAdded'] for server in result])
 
 
 def get_vm_backups(api_key=None, customer_id=None, vm_id=str):
-    url = api_link + 'vm/' + vm_id + '/backups'
+    url = '{0}/vm/{1}/backups/'.format(api_link, vm_id)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -171,17 +170,18 @@ def get_vm_backups(api_key=None, customer_id=None, vm_id=str):
     -> Printing result...
     '''.format(answer['status'])
 
-        pt= prettytable.PrettyTable()
-        result = answer['result']
+    pt = prettytable.PrettyTable()
+    result = answer['result']
 
-        pt.add_column('id', [server['id'] for server in result])
-        pt.add_column('size', [server['size'] for server in result])
-        # TODO: Convert Unix time to standard one.
-        pt.add_column('timeAdded', [server['timeAdded'] for server in result])
+    pt.add_column('id', [server['id'] for server in result])
+    pt.add_column('size', [server['size'] for server in result])
+    # TODO: Convert Unix time to standard one.
+    pt.add_column('timeAdded', [server['timeAdded'] for server in result])
 
 
 def get_pubkeys(api_key=None, customer_id=None):
     url = api_link + 'pubkeys'
+    url = '{0}/pubkeys/'.format(api_link)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -195,19 +195,19 @@ def get_pubkeys(api_key=None, customer_id=None):
     -> Printing result...
     '''.format(answer['status'])
 
-        pt= prettytable.PrettyTable()
-        result = answer['result']
+    pt = prettytable.PrettyTable()
+    result = answer['result']
 
-        pt.add_column('id', [server['id'] for server in result])
-        pt.add_column('name', [server['name'] for server in result])
-        pt.add_column('type', [server['type'] for server in result])
-        pt.add_column('publicKey', [server['publicKey'] for server in result])
-        # TODO: Convert Unix time to standard one.
-        pt.add_column('timeAdded', [server['timeAdded'] for server in result])
+    pt.add_column('id', [server['id'] for server in result])
+    pt.add_column('name', [server['name'] for server in result])
+    pt.add_column('type', [server['type'] for server in result])
+    pt.add_column('publicKey', [server['publicKey'] for server in result])
+    # TODO: Convert Unix time to standard one.
+    pt.add_column('timeAdded', [server['timeAdded'] for server in result])
 
 
 def get_software(api_key=None, customer_id=None):
-    url = api_link + 'software'
+    url = '{0}/software/'.format(api_link)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -221,15 +221,15 @@ def get_software(api_key=None, customer_id=None):
     -> Printing result...
     '''.format(answer['status'])
 
-        pt= prettytable.PrettyTable()
-        result = answer['result']
+    pt = prettytable.PrettyTable()
+    result = answer['result']
 
-        pt.add_column('id', [server['id'] for server in result])
-        pt.add_column('name', [server['name'] for server in result])
+    pt.add_column('id', [server['id'] for server in result])
+    pt.add_column('name', [server['name'] for server in result])
 
 
 def get_tariffs(api_key=None, customer_id=None):
-    url = api_link + 'tariffs'
+    url = '{0}/tariffs/'.format(api_link)
     if api_key or customer_id is None:
         api_key, customer_id = helpers.get_conf()
 
@@ -243,17 +243,17 @@ def get_tariffs(api_key=None, customer_id=None):
     -> Printing result...
     '''.format(answer['status'])
 
-        pt= prettytable.PrettyTable()
-        result = answer['result']
+    pt = prettytable.PrettyTable()
+    result = answer['result']
 
-        pt.add_column('id', [server['id'] for server in result])
-        pt.add_column('name', [server['name'] for server in result])
-        pt.add_column('memory', [server['memory'] for server in result])
-        pt.add_column('disk', [server['disk'] for server in result])
-        pt.add_column('cpu', [server['cpu'] for server in result])
-        pt.add_column('ipCount', [server['ipCount'] for server in result])
-        pt.add_column('onDemand', [server['onDemand'] for server in result])
-        pt.add_column('forWindows', [server['forWindows'] for server in result])
+    pt.add_column('id', [server['id'] for server in result])
+    pt.add_column('name', [server['name'] for server in result])
+    pt.add_column('memory', [server['memory'] for server in result])
+    pt.add_column('disk', [server['disk'] for server in result])
+    pt.add_column('cpu', [server['cpu'] for server in result])
+    pt.add_column('ipCount', [server['ipCount'] for server in result])
+    pt.add_column('onDemand', [server['onDemand'] for server in result])
+    pt.add_column('forWindows', [server['forWindows'] for server in result])
 
 
 if __name__ == '__main__':
