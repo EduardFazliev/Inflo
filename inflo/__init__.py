468262c5832e66c9dee9ee673c4b9994546c803a
efazliev <efazliev@infotech.team>
2016-08-12 11:43:48 +0300

    Add get_pubkeys to init.py.

--- inflo/__init__.py
+++ inflo/__init__.py
@@ -1,11 +1,11 @@
 from inflo.helpers import set_conf, get_conf
 from inflo.inflo_commands import get_tenant_info, server_list, os_list
 from inflo.inflo_commands import (
-    get_vm_info, get_vm_snapshots, get_vm_backups, get_software, get_tariffs, create_vm, delete_vm
+    get_vm_info, get_vm_snapshots, get_vm_backups, get_software, get_tariffs, create_vm, delete_vm, get_pubkeys
 )
 
 
 __all__ = [
     'set_conf', 'get_conf', 'get_tenant_info', 'server_list', 'os_list', 'get_vm_info', 'get_vm_snapshots',
-    'get_vm_backups', 'get_software', 'get_tariffs', 'create_vm', 'delete_vm'
+    'get_vm_backups', 'get_software', 'get_tariffs', 'create_vm', 'delete_vm', 'get_pubkeys'
 ]
