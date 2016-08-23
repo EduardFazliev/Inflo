8ad35484595cb958122f3c0f5481b9cdb9db26cb
efazliev <efazliev@infotech.team>
2016-08-23 20:24:21 +0300

    Remove useless files.

--- inflo/__init__.py
+++ inflo/__init__.py
@@ -2,6 +2,5 @@ from inflo.flops import FlopsApi
 from inflo.parser import invoke_parser
 
 __all__ = [
-    'set_conf', 'get_conf', 'create_vm', 'delete_vm', 'invoke_parser', 'start_server', 'wait_for_async_answer',
-    'get_info', 'FlopsApi'
+    'FlopsApi'
 ]
